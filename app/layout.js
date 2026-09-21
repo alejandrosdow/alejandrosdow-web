85a389e1ee213c0aa35b1b7b3fcb7fc698a0d8c6
alejandrosdow <alejandro.marcos@teamheretics.com>
2026-09-21 11:01:55 +0000

    favicon.ico for Google + structured data

--- app/layout.js
+++ app/layout.js
@@ -22,8 +22,30 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="es">
+      <body>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              '@context': 'https://schema.org',
+              '@graph': [
+                { '@type': 'WebSite', name: 'Alejandro Marcos', alternateName: 'alejandrosdow', url: 'https://alejandrosdow.com' },
+                {
+                  '@type': 'Person',
+                  name: 'Alejandro Marcos',
+                  url: 'https://alejandrosdow.com',
+                  image: 'https://alejandrosdow.com/assets/foto-alejandro.jpg',
+                  jobTitle: 'Chief Brand Officer',
+                  worksFor: { '@type': 'Organization', name: 'Team Heretics' },
+                  sameAs: ['https://x.com/alejandrosdow', 'https://instagram.com/alejandrosdow', 'https://linkedin.com/in/alejandromarcosmoraga', 'https://substack.com/@alejandrosdow'],
+                },
+              ],
+            }),
+          }}
+        />
+        {children}
+      </body>
     </html>
   );
 }
